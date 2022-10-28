794f2dffd4d06cd8ce38d39f9eb18a25a469c9a8
aatroxchen <792688960@qq.com>
2022-10-28 08:13:41 +0800

    New file:test.h and test.cpp

---
+++ BTree/include/test.h
@@ -0,0 +1,14 @@
+#include <SqBiTree.h>
+
+#ifndef __TEST_HEAD__
+#define __TEST_HEAD__
+
+Status BuildSBT(SqBiTree &T);
+void Test_InitSqBiTree(SqBiTree &T);
+void Test_DestroySBT(SqBiTree &T);
+void Test_InsertSBTNode(SqBiTree &T);
+void Test_ModifySBTNode(SqBiTree &T);
+void Test_BreakBiTree(SqBiTree &T);
+void Test_getDegreeN(SqBiTree &T);
+
+#endif
---
+++ BTree/src/test.cpp
@@ -0,0 +1,103 @@
+#include <test.h>
+
+Status BuildSBT(SqBiTree &T) // TODO:调整元素计算公式，使之只含字母
+{
+    if (IsIlleagl_SBT(T))
+        return ERROR;
+    int num = T.maxSize - 3;
+    TElemType e;
+    for (int i = 1; i <= num; i++)
+    {
+        e = (char)('a' + i - 1);
+        T.elem[i] = e;
+    }
+    T.lastIndex = num;
+    printf("OK:成功创建");
+    return OK;
+}
+
+void Test_InitSqBiTree(SqBiTree &T)
+{
+    int MaxDepth_SBT = 0;
+    printf(">>请输入二叉树最大层数\n");
+    scanf("%d", &MaxDepth_SBT);
+    printf(">>需要申请%d个单位元素的空间!\n", CountMaxNum(MaxDepth_SBT));
+    if (InitSqBiTree(T, MaxDepth_SBT) == OK)
+    {
+        printf("OK:初始化成功!\n");
+        ShowSBT(T);
+    }
+    else
+        printf("ERROR:初始化失败!\n");
+}
+
+void Test_DestroySBT(SqBiTree &T)
+{
+    if (DestroySBT(T) == OK)
+        printf("OK:销毁成功!\n");
+    else
+        printf("ERROR:销毁失败!请勿重复销毁.\n");
+}
+
+void Test_InsertSBTNode(SqBiTree &T)
+{
+    TElemType e, par;
+    char tag = '#';
+    printf(">>请输入要插入的新结点的值:\n");
+    fflush(stdin);
+    scanf("%c", &e);
+    printf(">>请输入要插入位置的双亲结点的值(若为根结点请输入'#'):\n");
+    fflush(stdin);
+    scanf("%c", &par);
+    printf(">>请输入要在%c下插入的方向:'L'或'R'(若为根结点请输入'#'):\n", par);
+    fflush(stdin);
+    scanf("%c", &tag);
+    if (InsertSBTNode(T, e, par, tag) == OK)
+        printf("OK:在%c下%c方向插入%c成功!\n", par, tag, e);
+    else
+        printf("ERROR:在%c下%c方向插入%c失败!非法或元素已重复\n", par, tag, e);
+}
+
+void Test_ModifySBTNode(SqBiTree &T)
+{
+    TElemType ori, e;
+    printf(">>请输入要修改的结点的的值:\n");
+    fflush(stdin);
+    scanf("%c", &ori);
+    printf(">>请输入修改后结点的值:\n");
+    fflush(stdin);
+    scanf("%c", &e);
+    if (ModifySBTNode(T, ori, e) == OK)
+        printf("OK:把值为%c的结点的值修改为%c成功!\n", ori, e);
+    else
+        printf("ERROR:把值为%c的结点的值修改为%c失败!\n", ori, e);
+}
+
+void Test_BreakBiTree(SqBiTree &T)
+{
+    SqBiTree L, R;
+    if (BreakBiTree(T, L, R) == OK)
+    {
+        printf("OK:分解成功!:\n");
+        printf("根如下:\n");
+        ShowSBT(T);
+        printf("左子树如下:\n");
+        ShowSBT(L);
+        printf("右子树如下:\n");
+        ShowSBT(R);
+        DestroySBT(L);
+        DestroySBT(R);
+    }
+    else
+    {
+        printf("ERROR:分解失败!\n");
+    }
+}
+
+void Test_getDegreeN(SqBiTree &T)
+{
+    printf("该二叉树度数为0的结点有:%d个\n", getDegreeZero(T));
+    printf("该二叉树度数为1的结点有:%d个\n", getDegreeOne(T));
+    printf("该二叉树度数为2的结点有:%d个\n", getDegreeTwo(T));
+    printf("该二叉树总结点数有:%d个\n", T.lastIndex);
+}
